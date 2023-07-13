d05ab24b9fe50a331b849071fd5e5217d2c4a478
Veritius <veritiusgaming@gmail.com>
2023-07-13 16:35:31 +1000

    More glam types

--- shared/serde/src/impls/glam.rs
+++ shared/serde/src/impls/glam.rs
@@ -1,6 +1,6 @@
 //! Trait implementations for glam types.
 
-use glam::{Affine2, Vec2, Vec3, Mat2, Mat3, Mat3A, Vec3A, Affine3A, BVec2, BVec3, BVec3A};
+use glam::{Affine2, Affine3A, BVec2, BVec3, BVec3A, Vec2, Vec3, Vec3A, Vec4, Mat2, Mat3, Mat3A, Mat4, Quat};
 use crate::serde::Serde;
 
 // TODO: The length of this file can be reduced significantly with macros.
@@ -122,18 +122,12 @@ impl Serde for Vec2 {
 
 impl Serde for Vec3 {
     fn ser(&self, writer: &mut dyn crate::BitWrite) {
-        self.x.ser(writer);
-        self.y.ser(writer);
-        self.z.ser(writer);
+        self.to_array().ser(writer);
     }
 
     fn de(reader: &mut crate::BitReader) -> Result<Self, crate::SerdeErr> {
-        let mut vec = Vec3::ZERO;
-        vec.x = read_f32(reader)?;
-        vec.y = read_f32(reader)?;
-        vec.z = read_f32(reader)?;
-
-        Ok(vec)
+        let array = <[f32;3] as Serde>::de(reader)?;
+        Ok(Vec3::from_array(array))
     }
 
     fn bit_length(&self) -> u32 {
@@ -155,16 +149,44 @@ impl Serde for Vec3A {
     }
 }
 
-impl Serde for Mat2 {
+impl Serde for Vec4 {
     fn ser(&self, writer: &mut dyn crate::BitWrite) {
-        self.x_axis.ser(writer);
-        self.y_axis.ser(writer);
+        self.to_array().ser(writer);
     }
 
     fn de(reader: &mut crate::BitReader) -> Result<Self, crate::SerdeErr> {
-        let x_axis = Vec2::de(reader)?;
-        let y_axis = Vec2::de(reader)?;
-        Ok(Mat2::from_cols(x_axis, y_axis))
+        let array = <[f32;4] as Serde>::de(reader)?;
+        Ok(Vec4::from_array(array))
+    }
+
+    fn bit_length(&self) -> u32 {
+        std::mem::size_of::<Self>() as u32 * 8
+    }
+}
+
+impl Serde for Quat {
+    fn ser(&self, writer: &mut dyn crate::BitWrite) {
+        self.to_array().ser(writer);
+    }
+
+    fn de(reader: &mut crate::BitReader) -> Result<Self, crate::SerdeErr> {
+        let array = <[f32;4] as Serde>::de(reader)?;
+        Ok(Quat::from_array(array))
+    }
+
+    fn bit_length(&self) -> u32 {
+        todo!()
+    }
+}
+
+impl Serde for Mat2 {
+    fn ser(&self, writer: &mut dyn crate::BitWrite) {
+        self.to_cols_array().ser(writer);
+    }
+
+    fn de(reader: &mut crate::BitReader) -> Result<Self, crate::SerdeErr> {
+        let array = <[f32;4] as Serde>::de(reader)?;
+        Ok(Mat2::from_cols_array(&array))
     }
 
     fn bit_length(&self) -> u32 {
@@ -174,16 +196,12 @@ impl Serde for Mat2 {
 
 impl Serde for Mat3 {
     fn ser(&self, writer: &mut dyn crate::BitWrite) {
-        self.x_axis.ser(writer);
-        self.y_axis.ser(writer);
-        self.z_axis.ser(writer);
+        self.to_cols_array().ser(writer);
     }
 
     fn de(reader: &mut crate::BitReader) -> Result<Self, crate::SerdeErr> {
-        let x_axis = Vec3::de(reader)?;
-        let y_axis = Vec3::de(reader)?;
-        let z_axis = Vec3::de(reader)?;
-        Ok(Mat3::from_cols(x_axis, y_axis, z_axis))
+        let array = <[f32;9] as Serde>::de(reader)?;
+        Ok(Mat3::from_cols_array(&array))
     }
 
     fn bit_length(&self) -> u32 {
@@ -193,16 +211,27 @@ impl Serde for Mat3 {
 
 impl Serde for Mat3A {
     fn ser(&self, writer: &mut dyn crate::BitWrite) {
-        self.x_axis.ser(writer);
-        self.y_axis.ser(writer);
-        self.z_axis.ser(writer);
+        self.to_cols_array().ser(writer);
     }
 
     fn de(reader: &mut crate::BitReader) -> Result<Self, crate::SerdeErr> {
-        let x_axis = Vec3A::de(reader)?;
-        let y_axis = Vec3A::de(reader)?;
-        let z_axis = Vec3A::de(reader)?;
-        Ok(Mat3A::from_cols(x_axis, y_axis, z_axis))
+        let array = <[f32;9] as Serde>::de(reader)?;
+        Ok(Mat3A::from_cols_array(&array))
+    }
+
+    fn bit_length(&self) -> u32 {
+        std::mem::size_of::<Self>() as u32 * 8
+    }
+}
+
+impl Serde for Mat4 {
+    fn ser(&self, writer: &mut dyn crate::BitWrite) {
+        self.to_cols_array().ser(writer);
+    }
+
+    fn de(reader: &mut crate::BitReader) -> Result<Self, crate::SerdeErr> {
+        let array = <[f32;16] as Serde>::de(reader)?;
+        Ok(Mat4::from_cols_array(&array))
     }
 
     fn bit_length(&self) -> u32 {
@@ -213,6 +242,7 @@ impl Serde for Mat3A {
 #[test]
 fn glam_type_equivalency_test() {
     use crate::{BitWriter, BitReader};
+    use glam::EulerRot;
     
     // Checks that T has the same value when it goes through serialisation/deserialisation.
     // Does not test for endian or other platform-related problem. May also suffer from fails due to floating points even if the bits are the same.
@@ -229,9 +259,19 @@ fn glam_type_equivalency_test() {
     }
 
     // Basically just runs through all the constants, because I can't think of good testing values.
-    type_equivalency_check::<Affine2>(&[Affine2::ZERO, Affine2::IDENTITY, Affine2::from_cols_array(&[0.0, 120.0, -15.0, 201423.0, 1019.0, -0.0])]);
-    type_equivalency_check::<Affine3A>(&[Affine3A::ZERO, Affine3A::IDENTITY, Affine3A::from_cols_array(&[0.0, 120.0, -15.0, 201423.0, 1019.0, -0.0, 5925.0, 0.000021, 102.0, 20.0, -250.0, -0.000052])]);
+    // This might need to be extended with more values.
+    type_equivalency_check::<Affine2>(&[Affine2::ZERO, Affine2::IDENTITY]);
+    type_equivalency_check::<Affine3A>(&[Affine3A::ZERO, Affine3A::IDENTITY]);
+    type_equivalency_check::<BVec2>(&[BVec2::TRUE, BVec2::FALSE]);
+    type_equivalency_check::<BVec3>(&[BVec3::TRUE, BVec3::FALSE]);
+    type_equivalency_check::<BVec3A>(&[BVec3A::TRUE, BVec3A::FALSE]);
     type_equivalency_check::<Vec2>(&[Vec2::ZERO, Vec2::ONE, Vec2::NEG_ONE, Vec2::X, Vec2::Y, Vec2::NEG_X, Vec2::NEG_Y]);
     type_equivalency_check::<Vec3>(&[Vec3::ZERO, Vec3::ONE, Vec3::NEG_ONE, Vec3::X, Vec3::Y, Vec3::Z, Vec3::NEG_X, Vec3::NEG_Y, Vec3::NEG_Z]);
     type_equivalency_check::<Vec3A>(&[Vec3A::ZERO, Vec3A::ONE, Vec3A::NEG_ONE, Vec3A::X, Vec3A::Y, Vec3A::Z, Vec3A::NEG_X, Vec3A::NEG_Y, Vec3A::NEG_Z]);
+    type_equivalency_check::<Vec4>(&[Vec4::ZERO, Vec4::ONE, Vec4::NEG_ONE, Vec4::X, Vec4::Y, Vec4::Z, Vec4::W, Vec4::NEG_X, Vec4::NEG_Y, Vec4::NEG_Z, Vec4::NEG_W]);
+    type_equivalency_check::<Quat>(&[Quat::from_euler(EulerRot::XYZ, 0.0, 0.0, 0.0)]);
+    type_equivalency_check::<Mat2>(&[Mat2::ZERO, Mat2::IDENTITY]);
+    type_equivalency_check::<Mat3>(&[Mat3::ZERO, Mat3::IDENTITY]);
+    type_equivalency_check::<Mat3A>(&[Mat3A::ZERO, Mat3A::IDENTITY]);
+    type_equivalency_check::<Mat4>(&[Mat4::ZERO, Mat4::IDENTITY]);
 }
